c4f3505aca3838dd5a577c4e0392022b128b2ca9
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2017-05-20 15:40:25 +0200

    Verification form when the user submits the registration form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,22 @@
 class User < ApplicationRecord
 
   has_secure_password
+  has_secure_token :conformation_token
+
+  validates :username, format: {
+      with: /\A[a-zA-Z0-9_]{2,20}\z/,
+      message: " ne doit contenir que des caractères alphanumériques ou des _"
+  }, uniqueness: {
+      case_sensitive: false,
+      message: " Il éxiste déjà un compte avec ce pseudo"
+  }
+
+  validates :email, format: {
+      with: /\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/,
+      message: "n'a pas l'air d'être un email"
+  }, uniqueness: {
+      case_sensitive: false,
+      message: " Il éxiste déjà un compte avec ce mail"
+  }
 
 end

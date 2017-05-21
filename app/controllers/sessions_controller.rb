e9f751b388fe6c6024d43686dd63964a39e4be60
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2017-05-21 10:26:26 +0200

    Template with form for user connection and creattion Sessions's controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,14 @@
+class SessionsController < ApplicationController
+
+  skip_before_action :only_signed_in, only: [:new, :create]
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+end

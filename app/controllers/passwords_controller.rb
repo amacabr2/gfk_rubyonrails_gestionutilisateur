4326a73a4e017a90719bcf98d5400a75c0ec4c61
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2017-05-21 14:43:59 +0200

    The user can change password via a system of e-mail

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -3,6 +3,7 @@ class PasswordsController < ApplicationController
   skip_before_action :only_signed_in
 
   before_action :only_signed_out
+  before_action :get_params_user, only: [:edit, :update]
 
   def new
 
@@ -18,7 +19,35 @@ class PasswordsController < ApplicationController
     else
       redirect_to new_password_path, danger: "Aucun utilisateur correspond à cet utilisateur"
     end
+  end
 
+  def edit
+    if @user.recover_password != params[:token]
+      redirect_to new_password_path, danger: 'Token invalide'
+    end
+  end
+
+  def update
+    user_params = params.require(:user).permit(:password, :password_confirmation, :recover_password)
+    if @user.recover_password == user_params[:recover_password]
+      @user.assign_attributes(user_params)
+      if @user.valid?
+        @user.recover_password = nil
+        @user.save
+        session[:auth] = @user.to_session
+        redirect_to profil_path, success: "Votre mot de passe a bien été modifié"
+      else
+        render :edit
+      end
+    else
+      redirect_to new_password_path, danger: 'Token invalide'
+    end
+  end
+
+  private
+
+  def get_params_user
+    @user = User.find(params[:id])
   end
 
 end
